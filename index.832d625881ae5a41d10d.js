facdeebc44321bd661bb6e875d172712b20497b3
Sidorov Maksim <maxx11167@gmail.com>
2023-07-17 18:20:09 +0300

    gh-pages

---
+++ index.832d625881ae5a41d10d.js
@@ -0,0 +1,2 @@
+(()=>{var e={562:()=>{console.log("Hello World!")}},r={};function o(t){var n=r[t];if(void 0!==n)return n.exports;var a=r[t]={exports:{}};return e[t](a,a.exports,o),a.exports}o.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return o.d(r,{a:r}),r},o.d=(e,r)=>{for(var t in r)o.o(r,t)&&!o.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:r[t]})},o.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),(()=>{"use strict";o(562)})()})();
+//# sourceMappingURL=index.832d625881ae5a41d10d.js.map
---
+++ pets.832d625881ae5a41d10d.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("pets ffff")})();
+//# sourceMappingURL=pets.832d625881ae5a41d10d.js.map
